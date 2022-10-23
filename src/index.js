534b5feddcbfa94712bb6e982821557d8dc9c11a
Pavel.K <pavelweb74@gmail.com>
2022-10-23 13:46:57 +0500

    add static content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const port = process.env.PORT || 3000;
+
+const getStaticContent = (res, pathFile, contentType = "text/html", status = 200) => {
+  const file = fs.readFile(path.resolve(__dirname, pathFile), (err, data) => {
+    console.log(`${__dirname}${path}`);
+    if (err) {
+      res.writeHead(404, {
+        "Content-type": "text/plain",
+      });
+      return res.end("Error");
+    }
+    res.writeHead(status, {
+      "Content-type": contentType,
+    });
+    res.end(data);
+  });
+};
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h2>test2</h2>");
+  switch (req.url) {
+    case "/about":
+      getStaticContent(res, "../public/about.html");
+      break;
+    case "/":
+      getStaticContent(res, "../public/home.html");
+      break;
+    default:
+      getStaticContent(res, "../public/404.html", "text/html", 404);
+      break;
+  }
 });
 
 server.listen(port, () => console.log(`server started on ${port}`));
