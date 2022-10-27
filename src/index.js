1ab07c0d5fa49cc03a322d6c940fc08af2e57fab
Pavel.K <pavelweb74@gmail.com>
2022-10-27 20:28:49 +0500

    Delete unused code

--- src/index.js
+++ src/index.js
@@ -7,32 +7,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const getStaticContent = (res, pathFile, status = 200, contentType = "text/html") => {
-  fs.readFile(path.resolve(__dirname, pathFile))
-    .then(data => {
-      res.status(status);
-      res.type(contentType);
-      res.end(data);
-    })
-    .catch(err => {
-      res.status(404);
-      res.type("text/plain");
-      res.end("err");
-    });
-};
-
 app.use(express.static(path.resolve(__dirname, "../public")));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  // getStaticContent(res, "../public/home.html");
   res.render("home");
 });
 app.get("/about/", (req, res) => {
-  // console.log(req.params);
-  // getStaticContent(res, "../public/about.html");
   res.render("about");
 });
 app.use((req, res) => {
