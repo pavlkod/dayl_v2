b17f0419dff6eb8112512349ee58a50493058e80
Pavel.K <pavelweb74@gmail.com>
2022-10-31 20:09:35 +0500

    Add about handler test

--- test/handlers.test.js
+++ test/handlers.test.js
@@ -1,9 +1,19 @@
 const handlers = require("../src/handlers");
+
 test("test homepage", () => {
   const req = {};
   const res = { render: jest.fn() };
   handlers.home(req, res);
 
-  expect(res.render.mock.calls[0].length).toBe(2);
+  expect(res.render.mock.calls[0].length).toBe(1);
   expect(res.render.mock.calls[0][0]).toBe("home");
+});
+
+test("test aboutpage", () => {
+  const req = {};
+  const res = { render: jest.fn() };
+  handlers.about(req, res);
+
+  expect(res.render.mock.calls[0].length).toBe(2);
+  expect(res.render.mock.calls[0][0]).toBe("about");
 });
