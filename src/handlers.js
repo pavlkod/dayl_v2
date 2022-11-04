b92d4d1d6186db213028fa1a6e8bcf05b4df6623
Pavel.K <pavelweb74@gmail.com>
2022-11-04 17:09:29 +0500

    Add 404 handler

--- src/handlers.js
+++ src/handlers.js
@@ -1,2 +1,3 @@
 exports.home = (req, res) => res.render("home");
 exports.about = (req, res) => res.render("about", { test: "test" });
+exports.notFound = (req, res) => res.status(404).render("404");
