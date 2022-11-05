30c1fc97231229e473ecb4f7d8bd1f944a1a0d2f
Pavel.K <pavelweb74@gmail.com>
2022-11-05 16:57:57 +0500

    Add newsletter handlers

--- src/handlers.js
+++ src/handlers.js
@@ -1,3 +1,9 @@
 exports.home = (req, res) => res.render("home");
 exports.about = (req, res) => res.render("about", { test: "test" });
 exports.notFound = (req, res) => res.status(404).render("404");
+exports.newsletterSignup = (req, res) => res.render("newsletter-signup");
+exports.newsletterProcess = (req, res) => {
+  console.log(req.body);
+  res.redirect(303, "/newsletter-signup/thank-you");
+};
+exports.newsletterSuccess = (req, res) => res.render("newsletter-signup-thank-you");
