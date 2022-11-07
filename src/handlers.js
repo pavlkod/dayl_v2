3b2e2cd3540e2cd41ca6713149102e6900c03bbe
Pavel.K <pavelweb74@gmail.com>
2022-11-07 19:37:17 +0500

    Add newsletterAjax helper

--- src/handlers.js
+++ src/handlers.js
@@ -1,9 +1,18 @@
 exports.home = (req, res) => res.render("home");
 exports.about = (req, res) => res.render("about", { test: "test" });
 exports.notFound = (req, res) => res.status(404).render("404");
+
 exports.newsletterSignup = (req, res) => res.render("newsletter-signup");
 exports.newsletterProcess = (req, res) => {
   console.log(req.body);
   res.redirect(303, "/newsletter-signup/thank-you");
 };
 exports.newsletterSuccess = (req, res) => res.render("newsletter-signup-thank-you");
+
+exports.newsletterWithAjax = (req, res) => res.render("newsletter");
+exports.api = {
+  newsletterSuccess: (req, res) => {
+    console.log(req.body);
+    res.json({ result: `${req.body.name} was subscribed on ${req.body.email}` });
+  },
+};
