f7f3ba1c6d3558013509281957e73b09252b48fe
Pavel.K <pavelweb74@gmail.com>
2022-11-13 16:26:59 +0500

    Use req.cookies

--- src/handlers.js
+++ src/handlers.js
@@ -2,7 +2,11 @@ exports.home = (req, res) => res.render("home");
 exports.about = (req, res) => res.render("about", { test: "test" });
 exports.notFound = (req, res) => res.status(404).render("404");
 
-exports.newsletterSignup = (req, res) => res.render("newsletter-signup");
+exports.newsletterSignup = (req, res) => {
+  // res.cookie("test", "value2");
+  console.log(req.cookies);
+  res.render("newsletter-signup");
+};
 exports.newsletterProcess = (req, res) => {
   console.log(req.body);
   res.redirect(303, "/newsletter-signup/thank-you");
