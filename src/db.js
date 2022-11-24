741850b985b8582435bcc893b7e77640acb1d511
Pavel.K <pavelweb74@gmail.com>
2022-11-24 20:32:56 +0500

    Implement addVacationlnSeasonListener

--- src/db.js
+++ src/db.js
@@ -1,36 +1,9 @@
 const Vacation = require("./models/vacation");
+const vacationInSeasonListener = require("./models/vacationInSeasonListener");
 
 module.exports = {
-  getVacations: async (options = {}) => {
-    /*const vacations = [
-      {
-        name: "Однодневный тур в Худ-Ривер",
-        slug: "hood- river - day - trip",
-        category: "Однодневный тур",
-        sku: "HR199",
-        description:
-          "Проведите день в плавании по реке " +
-          "Колумбия и насладитесь сваренным " +
-          "по традиционным рецептам пивом в Худ-Ривер!",
-        location: {
-          search: "Худ-Ривер, Орегон, США",
-          price: 99.95,
-          tags: ["однодневный тур", "худ-ривер", "плавание", "виндсерфинг", "пивоварни"],
-          inSeason: true,
-          maximumGuests: 16,
-          available: true,
-          packagesSold: 0,
-        },
-      },
-    ];*/
-    const vacations = Vacation.find(options);
-    if (options.available !== undefined) return vacations.filter(({ available }) => available === options.available);
-    return vacations;
-  },
+  getVacations: async (options = {}) => Vacation.find(options),
   addVacationlnSeasonListener: async (email, sku) => {
-    // Мы только притворимся, что делаем это...
-    // Поскольку это асинхронная функция, автоматически
-    // будет возвращен новый промис,
-    // исполняющийся со значением undefined.
+    await vacationInSeasonListener.updateOne({ email }, { $push: { skus: sku } }, { upsert: true });
   },
 };
