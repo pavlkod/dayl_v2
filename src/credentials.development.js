d0a1a824f864534cffa238ba665318b7818723e7
Pavel.K <pavelweb74@gmail.com>
2022-11-27 16:52:39 +0500

    Add postgres connectionString

--- src/credentials.development.js
+++ src/credentials.development.js
@@ -5,7 +5,10 @@ module.exports = {
     pass: "!ec1#h%YghvX31t*YY",
   },
   mongo: {
-    connectionstring:
+    connectionString:
       "mongodb+srv://admin:admin@cluster0.b2dxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
+  postgres: {
+    connectionString: "postgres://bmgxknje:ztbnBC25uCtk5NhbaALVfIFIA0EcIzSR@lucky.db.elephantsql.com/bmgxknje",
   },
 };
