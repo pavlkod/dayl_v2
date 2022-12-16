ab7cfffa6d33f5af14a31543a89c889e047a1d9b
Pavel.K <pavelweb74@gmail.com>
2022-12-16 23:16:51 +0500

    Transfer newsletter-signup route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,8 @@ const handlers = require("../handlers");
 module.exports = app => {
   app.get("/", handlers.home);
   app.get("/about/", handlers.about);
+
+  app.get("/newsletter-signup/", handlers.newsletterSignup);
+  app.post("/newsletter-signup/process", handlers.newsletterProcess);
+  app.get("/newsletter-signup/thank-you", handlers.newsletterSuccess);
 };
