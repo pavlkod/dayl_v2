f4ac3d47636d2b58227599a23a4196514b828875
Pavel.K <pavelweb74@gmail.com>
2022-12-25 19:28:19 +0500

    Remove newsletter route from index.js

--- src/index.js
+++ src/index.js
@@ -58,9 +58,9 @@ routes(app);
 // app.get("/", handlers.home);
 // app.get("/about/", handlers.about);
 
-app.get("/newsletter-signup/", handlers.newsletterSignup);
-app.post("/newsletter-signup/process", handlers.newsletterProcess);
-app.get("/newsletter-signup/thank-you", handlers.newsletterSuccess);
+// app.get("/newsletter-signup/", handlers.newsletterSignup);
+// app.post("/newsletter-signup/process", handlers.newsletterProcess);
+// app.get("/newsletter-signup/thank-you", handlers.newsletterSuccess);
 
 app.get("/newsletter", handlers.newsletterWithAjax);
 app.post("/api/newsletter", handlers.api.newsletterSuccess);
